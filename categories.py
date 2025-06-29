1c38095d17a03116f285fb441e25c778ff61114d
Zasajin <147424818+Zasajin@users.noreply.github.com>
2025-06-29 23:56:26 +0200

    loading/saving categories from file

--- categories.py
+++ categories.py
@@ -1,16 +1,64 @@
 import list as sp_list
 import mainframe as mf
 
-ctgs = load_ctgs()
+def_ctgs = {
+
+    'Kitchen': 0,
+    'Gardening': 1,
+    'Spices': 2,
+    'Fish': 3,
+    'Meat': 4,
+    'Fruit and Veggie': 5,
+    'Vegan': 6,
+    'Frozen and Ice': 7,
+    'Bread': 8,
+    'Asian': 9,
+    'Pasta': 10,
+    'Canned Goods': 11,
+    'Baking': 12,
+    'Cheese': 13,
+    'Cereal': 14,
+    'Dairy': 15,
+    'Drinks': 16,
+    'Cleaning': 17,
+    'Bathroom': 18,
+    'Sweets': 19,
+
+}
 
 def load_ctgs():
 
-    pass
+    ctgs = {}
+
+    try:
+
+        with open('categories.txt', 'r') as file:
+
+            for line in file:
+
+                if '|' in line:
+
+                    category, priority = line.strip().split('|')
+                    ctgs[category.strip()] = int(priority.strip())
+
+            return ctgs
+    
+    except FileNotFoundError:
+
+        return def_ctgs.copy()
 
 
 def save_ctgs(ctgs):
 
-    pass
+    if ctgs is None:
+
+        ctgs is def_ctgs
+
+    with open('categories.txt', 'w') as file:
+
+        for category, priority in ctgs.items():
+
+            file.write(f'{category} | {priority}\n')
 
 
 def add_category(ctgs):
